622edea25cf8a689c924434e729565eb90281cb4
Kevin King <kcking@users.noreply.github.com>
2023-02-20 15:47:53 -0800

    Add nanogpt_rs.unique_characters

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -10,5 +12,11 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn nanogpt_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(unique_characters, m)?)?;
     Ok(())
+}
+
+#[pyfunction]
+fn unique_characters(s: &str) -> HashSet<char> {
+    HashSet::from_iter(s.chars())
 }
