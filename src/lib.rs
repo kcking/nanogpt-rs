89cee71376b50008cf559962a3ab392b8c0aa5c0
Kevin King <kcking@users.noreply.github.com>
2023-02-20 18:24:06 -0800

    Identity hash function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use ahash::HashSet;
+use std::{
+    collections::HashSet,
+    hash::{BuildHasher, Hasher},
+};
+
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -16,6 +20,34 @@ fn nanogpt_rs(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 #[pyfunction]
-fn unique_characters(s: &str) -> HashSet<char> {
+fn unique_characters(s: &str) -> HashSet<char, CharHasher> {
     HashSet::from_iter(s.chars())
+}
+
+#[derive(Debug, Clone, Default)]
+struct CharHasher {
+    c: char,
+}
+impl Hasher for CharHasher {
+    fn finish(&self) -> u64 {
+        self.c as _
+    }
+
+    fn write(&mut self, bytes: &[u8]) {
+        if bytes.len() != 4 {
+            return;
+        }
+        //  Gracefully fall-back to `0`
+        let i = u32::from_le_bytes(bytes.try_into().unwrap_or([0; 4]));
+        //  Gracefully fall-back to 'a'
+        self.c = char::from_u32(i).unwrap_or('a');
+    }
+}
+
+impl BuildHasher for CharHasher {
+    type Hasher = Self;
+
+    fn build_hasher(&self) -> Self::Hasher {
+        Self::default()
+    }
 }
